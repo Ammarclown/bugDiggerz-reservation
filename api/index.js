c252ecabedfa1b6ccf0370cfa8b1ad23057a2241
Ammarclown <ammar.mohamed@student.giu-uni.de>
2023-01-04 18:59:48 +0200

    added cors

--- api/index.js
+++ api/index.js
@@ -12,6 +12,12 @@ const { startKafkaProducer } = require('../connectors/kafka');
 
 // Config setup to parse JSON payloads from HTTP POST request body
 app.use(express.json());
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(express.urlencoded({ extended: false }));
 
 // Register the api routes
